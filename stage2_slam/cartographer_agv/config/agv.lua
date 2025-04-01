5168670f06eba7f7e76be1e8f16a4af005a82817
soonback212 <tnsqor212@gmail.com>
2025-04-01 16:55:34 +0900

    Update agv.lua

--- stage2_slam/cartographer_agv/config/agv.lua
+++ stage2_slam/cartographer_agv/config/agv.lua
@@ -1,9 +1,7 @@
-TRAJECTORY_BUILDER.pure_localization = false
-TRAJECTORY_BUILDER.use_imu_data = true
-TRAJECTORY_BUILDER.trajectory_builder_2d.use_imu_data = true
-TRAJECTORY_BUILDER.trajectory_builder_2d.use_odometry = true
+include "map_builder.lua"
+include "trajectory_builder.lua"
 
-return {
+options = {
   map_frame = "map",
   tracking_frame = "base_link",
   published_frame = "base_link",
@@ -12,6 +10,7 @@ return {
   use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
+  publish_frame_projected_to_2d = false,
   num_laser_scans = 1,
   laser_scan_topic = "scan",
   num_multi_echo_laser_scans = 0,
@@ -19,5 +18,21 @@ return {
   use_imu_data = true,
   imu_topic = "imu",
   odometry_topic = "odom",
-  ...
+  pose_publish_period_sec = 5e-3,
+  submap_publish_period_sec = 0.3,
+  trajectory_publish_period_sec = 30e-3,
+  map_builder = MAP_BUILDER,
+  trajectory_builder = TRAJECTORY_BUILDER,
 }
+
+MAP_BUILDER.use_trajectory_builder_2d = true
+
+TRAJECTORY_BUILDER_2D.use_imu_data = true
+TRAJECTORY_BUILDER_2D.use_odometry = true
+TRAJECTORY_BUILDER_2D.min_range = 0.1
+TRAJECTORY_BUILDER_2D.max_range = 12.0
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 1.0
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+
+return options
+
