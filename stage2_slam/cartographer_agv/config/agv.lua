f337e34424cc0e0dad9a54d2d910300d4da51818
soonback212 <tnsqor212@gmail.com>
2025-04-02 11:59:18 +0900

    Update agv.lua

--- stage2_slam/cartographer_agv/config/agv.lua
+++ stage2_slam/cartographer_agv/config/agv.lua
@@ -2,46 +2,42 @@ include "map_builder.lua"
 include "trajectory_builder.lua"
 
 options = {
+  map_builder = MAP_BUILDER,
+  trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
   tracking_frame = "base_link",
   published_frame = "base_link",
   odom_frame = "odom",
   provide_odom_frame = true,
+  publish_frame_projected_to_2d = false,
+  use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
-  publish_frame_projected_to_2d = false,
+  use_imu_data = true,
   num_laser_scans = 1,
-  laser_scan_topic = "scan",
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
-  use_imu_data = true,
-  imu_topic = "imu",
-  odometry_topic = "odom",
-  pose_publish_period_sec = 5e-3,
+  num_point_clouds = 0,
+  lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
-  trajectory_publish_period_sec = 30e-3,
-  rangefinder_sampling_ratio = 1.,
-  odometry_sampling_ratio = 1.,
-  fixed_frame_pose_sampling_ratio = 1.,
-  imu_sampling_ratio = 1.,
-  landmarks_sampling_ratio = 1.,
-  map_builder = MAP_BUILDER,
-  trajectory_builder = TRAJECTORY_BUILDER,
+  pose_publish_period_sec = 0.005,
+  trajectory_publish_period_sec = 0.03,
+  rangefinder_sampling_ratio = 1.0,
+  odometry_sampling_ratio = 1.0,
+  imu_sampling_ratio = 1.0,
+  fixed_frame_pose_sampling_ratio = 1.0,
+  landmarks_sampling_ratio = 1.0,
+  laser_scan_topic = "scan",
+  imu_topic = "imu",
+  odometry_topic = "odom"
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
--- 여기만 남기고, 위쪽 options 테이블엔 넣지 마!
 TRAJECTORY_BUILDER_2D.use_imu_data = true
-TRAJECTORY_BUILDER_2D.use_odometry = true
-TRAJECTORY_BUILDER_2D.min_range = 0.1
-TRAJECTORY_BUILDER_2D.max_range = 12.0
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 1.0
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
-TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
-
-POSE_GRAPH.constraint_builder.min_score = 0.65
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
+TRAJECTORY_BUILDER_2D.use_trajectory_builder_2d = true
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
 
 return options
 
