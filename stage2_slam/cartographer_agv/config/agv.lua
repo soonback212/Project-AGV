ba3014f48d9c5e9e36807797e6ec97e88e6fcc86
soonback212 <tnsqor212@gmail.com>
2025-04-02 14:20:49 +0900

    Update agv.lua

--- stage2_slam/cartographer_agv/config/agv.lua
+++ stage2_slam/cartographer_agv/config/agv.lua
@@ -6,29 +6,37 @@ MAP_BUILDER.use_trajectory_builder_2d = true
 options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
+
   map_frame = "map",
   tracking_frame = "base_link",
   published_frame = "base_link",
   odom_frame = "odom",
+
   provide_odom_frame = true,
   publish_frame_projected_to_2d = false,
+  publish_to_tf = true,
+
   use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
   use_imu_data = true,
+
   num_laser_scans = 1,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
+
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 0.005,
   trajectory_publish_period_sec = 0.03,
+
   rangefinder_sampling_ratio = 1.0,
   odometry_sampling_ratio = 1.0,
   imu_sampling_ratio = 1.0,
   fixed_frame_pose_sampling_ratio = 1.0,
   landmarks_sampling_ratio = 1.0,
+
   laser_scan_topic = "scan",
   imu_topic = "imu",
   odometry_topic = "odom"
@@ -39,4 +47,3 @@ TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
 
 return options
-
